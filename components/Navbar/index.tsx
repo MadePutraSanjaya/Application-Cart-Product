eda2d22da1717ac9626d0ef21c078dc343454268
I Made Adi Putra Sanjaya <madeputra.6166@gmail.com>
2024-06-04 22:51:45 +0800

    Update index.tsx

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -41,9 +41,6 @@ const Index = () => {
             <li>
               <Link className="font-light text-black navLink" href="/">Home</Link>
             </li>
-            <li>
-              <Link className="font-light text-black navLink" href="/product">Products</Link>
-            </li>
 
           </ul>
 
@@ -63,7 +60,7 @@ const Index = () => {
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                   </svg>
-                  <span className="badge badge-sm indicator-item">{totalItems}</span>
+                  <span className="badge badge-sm indicator-item bg-secondary2 text-white">{totalItems}</span>
                 </div>
               </div>
               <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
@@ -71,9 +68,9 @@ const Index = () => {
                   <span className="font-bold text-lg">{totalItems} Items</span>
                   <span className="text-info">Subtotal: ${subtotal.toFixed(2)}</span>
                   <div className="card-actions">
-                  <Link href="/cart">
-              <button className="btn btn-primary btn-block">View cart</button>
-            </Link>
+                    <Link href="/cart">
+                      <button className="btn btn-primary btn-block">View cart</button>
+                    </Link>
                   </div>
                 </div>
               </div>
@@ -95,37 +92,10 @@ const Index = () => {
               </ul>
             </div>
 
-            {/* Mobile Menu Button */}
-            <input id="my-drawer-3" type="checkbox" className="drawer-toggle lg:hidden" />
-            <div className="lg:hidden">
-              <label htmlFor="my-drawer-3" aria-label="open sidebar" className="text-black btn btn-square btn-ghost">
-                <svg id="hamburger" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-                </svg>
-              </label>
-            </div>
+           
           </div>
 
-          {/* Mobile Drawer Menu */}
-          <div className="drawer-side lg:hidden">
-            <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
-            <ul className="min-h-full p-4 space-y-3 menu w-80 bg-base-100">
-              <li>
-                <a id="drawer-link" className="font-light text-black" href="/">Home</a>
-              </li>
-              <li>
-                <a id="drawer-link" className="font-light text-black" href="/about">About Us</a>
-              </li>
-              <li>
-                <a id="drawer-link" className="font-light text-black" href="/itinerary">Itinerary</a>
-              </li>
-              <li>
-                <a id="drawer-link" className="font-light text-black" href="/services">Services</a>
-              </li>
-              <li>
-                <a id="drawer-link" className="font-light text-black" href="/contact">Contact</a>
-              </li>
-            </ul>Link          </div>
+         
         </div>
       </div>
     </div>
