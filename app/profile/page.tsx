10b28b47121ebe66c41aab4c3b8a8f7162859869
I Made Adi Putra Sanjaya <madeputra.6166@gmail.com>
2024-06-17 18:14:47 +0800

    update

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -12,10 +12,10 @@ const Profile = () => {
     <div className='relative px-6 py-10 mx-auto sm:px-16 sm:py-24 max-w-7xl overflow-x-hidden'>
       <Title level={1} className='text-text2'>My Account</Title>
       <div className="grid grid-cols-2 gap-6">
-        <Input value='Made Sanjaya' disabled title="Username" />
-        <Input value='madeputra.6166@gmail.com' title="Email" disabled />
-        <Input value='085942965516' title="Handphone" disabled />
-        <Index title="Gender" initialValue={1} options={radioOptions} disabled />
+        <Input value=' ' disabled title="Username" />
+        <Input value=' ' title="Email" disabled />
+        <Input value=' ' title="Handphone" disabled />
+        <Index title="Gender" initialValue={3} options={radioOptions} disabled />
       </div>
     </div>
   )
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -87,7 +87,7 @@ const Index = () => {
               </div>
               <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
                 <li>
-                  <Link href="/profile" className="justify-between">
+                  <Link href="/" className="justify-between">
                     Profile
                     {/* <span className="badge">New</span> */}
                   </Link>
